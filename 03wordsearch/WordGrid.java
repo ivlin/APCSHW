1fc4fa02ee0d30be8909f1955fc5b8e7d634e128
ivlin <ivlin112@gmail.com>
2014-11-19 18:33:38 -0500

    Wrote basic constructor, clear, toString for WordGrid

---
+++ 03wordsearch/WordGrid.java
@@ -0,0 +1,57 @@
+public class WordGrid{
+    private char[][]data;
+
+    public static void main(String[]args){
+	WordGrid a = new WordGrid(4,6);
+	System.out.println(a);
+    }
+
+
+    /**Initialize the grid to the size specified and fill all of the positions
+     *with spaces.
+     *@param row is the starting height of the WordGrid
+     *@param col is the starting width of the WordGrid
+     */
+    public WordGrid(int rows,int cols){
+	data = new char[rows][cols];
+    }
+
+    /**Set all values in the WordGrid to spaces ' '*/
+    private void clear(){
+	for (int x = 0; x < data.length; x++){
+	    for (int y = 0; y < data[x].length; y++){
+		data[x][y] = ' ';
+	    }
+	}
+    }
+
+    /**The proper formatting for a WordGrid is created in the toString.
+     *@return a String with each character separated by spaces, and each row
+     *separated by newlines.
+     */
+    public String toString(){
+	String str = "";
+	for (int x = 0; x < data.length; x++){
+	    for (int y = 0; y < data[x].length; y++){
+		str += data[x][y];
+	    }
+	    str += '\n';
+	}
+	return str;
+    }
+
+    /**Attempts to add a given word to the specified position of the WordGrid.
+     *The word is added from left to right, must fit on the WordGrid, and must
+     *have a corresponding letter to match any letters that it overlaps.
+     *
+     *@param word is any text to be added to the word grid.
+     *@param row is the vertical locaiton of where you want the word to start.
+     *@param col is the horizontal location of where you want the word to start.
+     *@return true when the word is added successfully. When the word doesn't fit,
+     *or there are overlapping letters that do not match, then false is returned.
+     */
+    //public boolean addWordHorizontal(String word,int row, int col){	
+	//    }
+
+    //vertical + diagonal should be implemented as well.
+}
