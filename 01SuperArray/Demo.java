3fb52498576c330da9b9b2eb26ba134df0edfecd
ivan <ivlin112@gmail.com>
2014-12-08 21:11:21 -0500

    Removed main and put in into Demo

--- 01SuperArray/Demo.java
+++ 01SuperArray/Demo.java
@@ -4,13 +4,29 @@ public class Demo{
 	SuperArray a = new SuperArray();
 	System.out.println(a);
 	System.out.println(a.size());
-	for (int i = 0; i < 12; i++){
+	a.add();
+	a.set(0,"first");
+	a.add(1,"second");
+	try{
+	    a.set(1,"hi");
+	    a.get(100);
+	}
+	catch (IndexOutOfBoundsException e){
+	    System.out.println("Index out of bounds");
+	}
+	System.out.println(a);
+	for (int i = 0; i < 400; i ++){
 	    a.add(i);
 	}
 	System.out.println(a);
-	System.out.println(a.size());
-	a.clear();
+	System.out.println(a.get(4));
+	System.out.println(a.get(15));
+	for (int i = 0; i < 399; i ++){
+	    a.remove(2);
+	}
 	System.out.println(a);
+
     }
+
 
 }
--- 01SuperArray/SuperArray.java
+++ 01SuperArray/SuperArray.java
@@ -2,34 +2,6 @@ public class SuperArray{
     private Object[]array;
     private int length;
 
-    public static void main(String[]args){
-	SuperArray a = new SuperArray();
-	System.out.println(a);
-	System.out.println(a.size());
-	a.add();
-	a.set(0,"first");
-	a.add(1,"second");
-	try{
-	    a.set(1,"hi");
-	    a.get(100);
-	}
-	catch (IndexOutOfBoundsException e){
-	    System.out.println("Index out of bounds");
-	}
-	System.out.println(a);
-	for (int i = 0; i < 400; i ++){
-	    a.add(i);
-	}
-	System.out.println(a);
-	System.out.println(a.get(4));
-	System.out.println(a.get(15));
-	for (int i = 0; i < 399; i ++){
-	    a.remove(2);
-	}
-	System.out.println(a);
-
-    }
-
     public SuperArray(){
 	this(10);
     }
