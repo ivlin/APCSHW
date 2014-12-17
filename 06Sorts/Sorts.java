cd26db56ec0d3d12c1129901b4ad54a2f3369653
ivan <ivlin112@gmail.com>
2014-12-16 22:26:23 -0500

    Added the selection and insertion sorts, and tested them in the main of Sorts.java

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -1,18 +1,23 @@
+import java.util.*;
 public class Sorts{
-   
+
     public static void main(String[] args){
+	Random rand = new Random();
 	int[]array = new int[10];
 	for (int i = 0; i < 10; i++){
-	    array[i] = i;
+	    array[i] = rand.nextInt(50);
 	}
-	for (int i = 0; i < 10; i++){
-	    System.out.print(i);
-	}
+	System.out.println(Arrays.toString(array));
+	//Sorts.bubble(array);
+	//Sorts.selection(array);
+	//Sorts.insertion(array);
+	System.out.println(Arrays.toString(array));
+
     }
 
     public static void bubble(int[] c){
 	for (int x = 0; x < c.length; x ++){
-	    for (int i = x; i < c.length; i ++){
+	    for (int i = 0; i < c.length - x - 1; i ++){
 		if (c[i] > c[i + 1]){
 		    int hold = c[i];
 		    c[i] = c[i + 1];
@@ -20,6 +25,36 @@ public class Sorts{
 		}
 	    }
 	}	
+    }
+
+
+    public static void insertion(int[] c){
+	for (int x = 0; x < c.length;x++){
+	    int i = x;
+	    while (i >= 0 && c[x] <= c[i]){
+		i--;
+	    }
+	    int hold = c[x];
+	    for (int n = i + 1; n <= x; n ++){
+		int z = c[n];
+		c[n] = hold;
+		hold = z;
+	    }
+	}
+    }
+
+    public static void selection(int[] c){
+	for (int i = 0; i < c.length; i ++){
+	    int lowest = i;
+	    for (int x = i; x < c.length; x ++){
+		if (c[x] < c[lowest]){
+		    lowest = x;
+		}
+	    }
+	    int replaced = c[i];
+	    c[i] = c[lowest];
+	    c[lowest] = replaced;
+	}
     }
 }
     
