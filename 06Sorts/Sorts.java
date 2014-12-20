8ac63c473cef7c05c77b2f4e2f2130cb635496ee
ivan <ivlin112@gmail.com>
2014-12-19 22:13:19 -0500

    Made the part of radix with placing bins in the buckets and moving them back. Problems with doing it more than once.

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -1,6 +1,16 @@
 import java.util.*;
 public class Sorts{
 
+    public static void main(String[] args){
+	int [] a = new int[10];
+	Random rand = new Random();
+	for (int i = 0; i < a.length; i++){
+	    a[i] = rand.nextInt(100);
+	}
+	System.out.println(Arrays.toString(a));
+	Sorts.radix(a);
+    }
+
     public static void bubble(int[] c){
 	int hold;
 	for (int x = 0; x < c.length; x ++){
@@ -47,15 +57,34 @@ public class Sorts{
     }
 
     public static void radix(int[] c){
-	ArrayList<ArrayList<Integer>> b = new ArrayList<ArrayList<Integer>>(10);
-	for (int x = 0; x < c.length; x++){
-	    b.add(c[x] % 10, c[x])
+	ArrayList<ArrayList<Integer>> b = new ArrayList<ArrayList<Integer>>();
+	for (int r = 0; r < 10; r++){
+	    b.add(new ArrayList<Integer>());
 	}
-	for (ArrayList<Integer> a: b){
-	    for (Integer temp: a){
-		
+	int  max = c[0];
+	for (int f = 1; f < c.length; f++){
+	    if (c[f] > max){
+		max = c[f];
 	    }
-	} 
+	}
+	int digits = 1;
+	while (max > 1){
+	    max = max / 10;
+	    for (int i = 0; i < c.length; i++){
+		b.get(c[i] % (int)(Math.pow(10, digits))).add(c[i]);
+	    }
+	    int returns = 0;
+	    System.out.println(b.toString());
+	    for (ArrayList<Integer> r : b){
+		for (Integer n : r){
+		    c[returns] = n;
+		    returns++;
+		}
+	    }
+	    digits ++;
+	    break;
+	}
+	System.out.println(Arrays.toString(c));
     }
 }
-    
+
