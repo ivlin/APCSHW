077aa1ef9ad00328a7725f6cd2feb49dc98ff570
ivan <ivlin112@gmail.com>
2014-12-20 13:20:52 -0500

    Finished radix and tested it. Seems to be faster than insertion until elements' range reaches 7 digits.

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -1,16 +1,6 @@
 import java.util.*;
 public class Sorts{
 
-    public static void main(String[] args){
-	int [] a = new int[10];
-	Random rand = new Random();
-	for (int i = 0; i < a.length; i++){
-	    a[i] = rand.nextInt(100);
-	}
-	System.out.println(Arrays.toString(a));
-	Sorts.radix(a);
-    }
-
     public static void bubble(int[] c){
 	int hold;
 	for (int x = 0; x < c.length; x ++){
@@ -57,34 +47,32 @@ public class Sorts{
     }
 
     public static void radix(int[] c){
+	//makes buckets
 	ArrayList<ArrayList<Integer>> b = new ArrayList<ArrayList<Integer>>();
 	for (int r = 0; r < 10; r++){
 	    b.add(new ArrayList<Integer>());
 	}
+	//find max
 	int  max = c[0];
 	for (int f = 1; f < c.length; f++){
 	    if (c[f] > max){
 		max = c[f];
 	    }
 	}
-	int digits = 1;
-	while (max > 1){
-	    max = max / 10;
+	int returns;
+	for (int digit = 1; digit <= ("" + max).length(); digit ++){
 	    for (int i = 0; i < c.length; i++){
-		b.get(c[i] % (int)(Math.pow(10, digits))).add(c[i]);
+		b.get((int)(Math.floor(c[i] / Math.pow(10, digit - 1)) % 10)).add(c[i]);
 	    }
-	    int returns = 0;
-	    System.out.println(b.toString());
+	    returns = 0;
 	    for (ArrayList<Integer> r : b){
 		for (Integer n : r){
 		    c[returns] = n;
 		    returns++;
 		}
+		r.clear();
 	    }
-	    digits ++;
-	    break;
 	}
-	System.out.println(Arrays.toString(c));
     }
 }
 
